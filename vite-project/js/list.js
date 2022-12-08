aa7f9a8d9ff02fb422e28a226eaf28c4b305eb26
pengshi2 <boogiesnoogawoot@gmail.com>
2022-12-08 10:10:33 -0500

    bb

---
+++ vite-project/js/list.js
@@ -0,0 +1,102 @@
+const menu = [
+  {
+    name: "Steak",
+    meatType: "beef",
+    description: "Fine Steak",
+    price: 20,
+    vegetarian: false,
+    image: "",
+    rating: "5star",
+  },
+
+  {
+    name: " Cheese Pizza",
+    meatType: "none",
+    description: "Good Italian Pizza",
+    price: 12,
+    vegetarian: true,
+    image: "",
+    rating: "4star",
+  },
+
+  {
+    name: "Mozzerella Sticks",
+    meatType: "none",
+    description: "Delicous Cheesy Mozzerella Sticks",
+    price: 8,
+    vegetarian: true,
+    image: "",
+    rating: "3star",
+  },
+
+  {
+    name: "Cheeseburger",
+    meatType: "beef",
+    description: "Juicy Hot Cheeseburger",
+    price: 10,
+    vegetarian: false,
+    image: "",
+    rating: "5star",
+  },
+
+  {
+    name: "Sushi",
+    meatType: "fish",
+    description: "Delicous Japanese Sushi",
+    price: 20,
+    vegetarian: false,
+    rating: "5star",
+    image: "",
+  },
+
+  {
+    name: "French Fries",
+    meatType: "none",
+    description: "Crunchy Salty Fries",
+    price: 6,
+    vegetarian: true,
+    image: "",
+    rating: "3star",
+  },
+  {
+    name: "Apple Pie",
+    meatType: "none",
+    description: "Freshly Baked Apple Pies",
+    price: 15,
+    vegetarian: true,
+    rating: "4star",
+    image: "",
+  },
+
+  {
+    name: "Fried Chicken",
+    meatType: "chicken",
+    description: "Crunchy Juicy Fried Chicken",
+    price: 14,
+    vegetarian: false,
+    rating: "4star",
+    image: "",
+  },
+
+  {
+    name: "Stir-Fried Beef",
+    meatType: "beef",
+    description: "Amazing Chinese Stir-fried beef",
+    price: 10,
+    vegetarian: false,
+    rating: "5star",
+    image: "",
+  },
+
+  {
+    name: "Chicken Quesadilla",
+    meatType: "chicken",
+    description: "Spicy Mexican Quesadilla",
+    price: 12,
+    vegetarian: false,
+    image: "",
+    rating: "4star",
+  },
+];
+
+console.log(menu);
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,13 +1,4 @@
-// document.getElementById("theme").addEventListener("click", function () {
-//   if (document.body.classList.contains("light")) {
-//     document.body.classList.add("dark");
-//     document.body.classList.remove("light");
-//   } else {
-//     document.body.classList.add("light");
-//     document.body.classList.remove("dark");
-//   }
-// });
-
+function cool();
 document.getElementById("theme").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
     document.body.classList = ["dark"];
