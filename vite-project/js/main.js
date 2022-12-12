71b1d8b661e3ea6fbda5ddb6f13e8c138cf72936
pengshi2 <boogiesnoogawoot@gmail.com>
2022-12-12 10:11:46 -0500

    b

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,23 +1,31 @@
-import { menu } from "./list.js";
+// const menuDiv = document.querySelector("#menu");
+// display(menu);
 
-const menuDiv = document.querySelector("#menu");
-display(menu);
+// document.querySelector("#all").addEventListener("click", function () {
+//   menuDiv.innerHTML = ``;
+//   display(menu);
+// });
 
-document.querySelector("#all").addEventListener("click", function () {
-  menuDiv.innerHTML = ``;
-  display(menu);
-});
+// document.querySelector("beef").addEventListener("click", function () {
+//   menuDiv.innerHTML = "";
+//   const filtered = menu.filter((item) => item.meat === "beef");
+//   display(filtered);
+// });
 
-document.querySelector("beef").addEventListener("click", function () {
-  menuDiv.innerHTML = "";
-  const filtered = menu.filter((item) => item.meat === "beef");
-  display(filtered);
-});
+// document.querySelector("#theme").addEventListener("click", function () {
+//   if (document.body.classList.contains("light")) {
+//     document.body.classList = ["dark"];
+//   } else {
+//     document.body.classList = ["light"];
+//   }
+// });
 
-document.querySelector("#theme").addEventListener("click", function () {
+document.querySelector(".theme").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
-    document.body.classList = ["dark"];
+    document.body.classList.add("dark");
+    document.body.classList.remove("dark");
   } else {
-    document.body.classList = ["light"];
+    document.body.classList.add("light");
+    document.body.classList.remove("warm");
   }
 });
