51070d6ad01f0d083ad8b52f0a93bc195afafd1b
pengshi2 <boogiesnoogawoot@gmail.com>
2022-12-13 10:10:25 -0500

    b

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,31 +1,7 @@
-// const menuDiv = document.querySelector("#menu");
-// display(menu);
-
-// document.querySelector("#all").addEventListener("click", function () {
-//   menuDiv.innerHTML = ``;
-//   display(menu);
-// });
-
-// document.querySelector("beef").addEventListener("click", function () {
-//   menuDiv.innerHTML = "";
-//   const filtered = menu.filter((item) => item.meat === "beef");
-//   display(filtered);
-// });
-
-// document.querySelector("#theme").addEventListener("click", function () {
-//   if (document.body.classList.contains("light")) {
-//     document.body.classList = ["dark"];
-//   } else {
-//     document.body.classList = ["light"];
-//   }
-// });
-
-document.querySelector(".theme").addEventListener("click", function () {
+document.getElementById("theme-button").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
-    document.body.classList.add("dark");
-    document.body.classList.remove("dark");
+    document.body.classList = ["dark"];
   } else {
-    document.body.classList.add("light");
-    document.body.classList.remove("warm");
+    document.body.classList = ["light"];
   }
 });
