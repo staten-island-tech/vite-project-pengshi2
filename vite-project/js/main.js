aabe5d012480aa3759476b4e8658ee09a79f4148
pengshi2 <boogiesnoogawoot@gmail.com>
2022-12-14 10:07:08 -0500

    b

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -5,3 +5,20 @@ document.getElementById("theme-button").addEventListener("click", function () {
     document.body.classList = ["light"];
   }
 });
+
+const menuDiv = document.querySelector("#menu");
+display(menu);
+
+function display(menu) {
+  forEach((item) => {
+    menuDiv.insertAdjacentHTML(
+      "beforeend",
+      `<div class="menu-object">
+              <h2>${item.name}</h2>
+              <img class="menu-image" src="${item.image}">
+              <p class="menu-object-price">$${item.price}</p>
+              <p class="menu-object-text">${item.description}</p>
+          </div>`
+    );
+  });
+}
