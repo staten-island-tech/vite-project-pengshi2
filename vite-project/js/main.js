a619658925330db59f499c2fb2deb34327be7a60
pengshi2 <boogiesnoogawoot@gmail.com>
2022-12-16 10:13:17 -0500

    b

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,4 @@
-import { menu } from "./js/menu.js";
-np;
+import { menu } from "./list";
 
 document.getElementById("theme-button").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
@@ -9,17 +8,14 @@ document.getElementById("theme-button").addEventListener("click", function () {
   }
 });
 
-const menuDiv = document.querySelector("#menu");
-display(menu);
-
-function test() {
-  forEach((item) => {
+function foo() {
+  menu.forEach((item) => {
     menuDiv.insertAdjacentHTML(
       "beforeend",
       `<div class="items">
               <h2>${item.name}</h2>
               <img class="item-image" src="${item.image}">
-              <p class="item-price">$${item.price}</p>
+              <p class="item-price">${item.price}</p>
               <p class="item-description">${item.description}</p>
           </div>`
     );
